31b15cf19a45d2da888d96144d8bf8ecdc49f23f
Grant <gvvuur3n@gmail.com>
2019-11-22 12:13:27 +0100

    Update

--- src/HardwareLayer/Notification/Speaker.java
+++ src/HardwareLayer/Notification/Speaker.java
@@ -2,9 +2,29 @@ package HardwareLayer.Notification;
 
 import HardwareLayer.Switchable;
 import HeadInterfaces.Updatable;
+import TI.BoeBot;
+import TI.PWM;
 
 public class Speaker implements Updatable, Switchable {
+
     private boolean isOn;
+    private int pin;
+    private PWM speakerPWM;
+    private int speakerFrequency;
+
+
+    public Speaker( ){
+
+        this.pin = 6;//GPIO pin number for the speaker.
+        this.speakerPWM = new PWM( pin,0);
+        this.speakerFrequency = 0;
+    }
+
+    public void speakerFrequencyUpdate( int frequency ){
+
+        this.speakerFrequency = frequency;
+
+    }
 
     @Override
     public boolean isOn() {
@@ -16,13 +36,20 @@ public class Speaker implements Updatable, Switchable {
 
     }
 
+
     @Override
     public void on() {
+
+        this.speakerPWM = new PWM( this.pin, this.speakerFrequency );
         isOn = true;
+
     }
 
     @Override
     public void off() {
+
+        this.speakerPWM = new PWM( this.pin, this.speakerFrequency );
         isOn = false;
+
     }
 }
