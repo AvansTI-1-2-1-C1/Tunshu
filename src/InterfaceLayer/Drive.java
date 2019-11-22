8a2f94d3e84ef2e8bbb4c4db7cd720d808c742ac
Thaomas <thaomas35@gmail.com>
2019-11-22 11:16:32 +0100

    Motor aanmaken

--- src/InterfaceLayer/Drive.java
+++ src/InterfaceLayer/Drive.java
@@ -1,15 +1,23 @@
 package InterfaceLayer;
 
+import HardwareLayer.Motor;
 import TI.BoeBot;
-import TI.Servo;
 
 public class Drive{
 
-    private Servo left;
-    private Servo right;
+    private Motor left;
+    private Motor right;
     private int speed;
     private boolean forwards;
     private boolean backwards;
+
+    public Drive(int servoLeft, int servoRight) {
+        this.left = new Motor(servoLeft);
+        this.right = new Motor(servoRight);
+        this.speed = 0;
+        this.forwards = true;
+        this.backwards = true;
+    }
 
     public void control(int button) {
         switch (button) {
@@ -90,30 +98,19 @@ public class Drive{
         }
     }
 
-
-    public Drive(int servoLeft, int servoRight) {
-        this.left = new Servo(servoLeft);
-        this.right = new Servo(servoRight);
-        this.left.start();
-        this.right.start();
-        this.speed = 0;
-        this.forwards = true;
-        this.backwards = true;
-    }
-
     public void accelerate(int oldSpeed) {
         if (this.forwards) {
             while (oldSpeed < this.speed) {
                 oldSpeed++;
-                this.left.update(1500 + oldSpeed);
-                this.right.update(1500 - oldSpeed);
+                this.left.setSpeed(1500 + oldSpeed);
+                this.right.setSpeed(1500 - oldSpeed);
                 BoeBot.wait(10);
             }
         } else if (this.backwards) {
             while (oldSpeed < this.speed) {
                 oldSpeed++;
-                this.left.update(1500 - oldSpeed);
-                this.right.update(1500 + oldSpeed);
+                this.left.setSpeed(1500 - oldSpeed);
+                this.right.setSpeed(1500 + oldSpeed);
                 BoeBot.wait(10);
             }
         }
@@ -123,15 +120,15 @@ public class Drive{
         if (this.backwards) {
             while (oldSpeed > this.speed) {
                 oldSpeed--;
-                this.left.update(1500 - oldSpeed);
-                this.right.update(1500 + oldSpeed);
+                this.left.setSpeed(1500 - oldSpeed);
+                this.right.setSpeed(1500 + oldSpeed);
                 BoeBot.wait(10);
             }
         } else if (this.forwards) {
             while (oldSpeed > this.speed) {
                 oldSpeed--;
-                this.left.update(1500 + oldSpeed);
-                this.right.update(1500 - oldSpeed);
+                this.left.setSpeed(1500 + oldSpeed);
+                this.right.setSpeed(1500 - oldSpeed);
                 BoeBot.wait(10);
             }
         }
@@ -140,36 +137,36 @@ public class Drive{
 
     public void turnLeft() {
         if (this.speed > 0) {
-            this.right.update(1450 - this.speed);
+            this.right.setSpeed(1450 - this.speed);
             BoeBot.wait(850);
-            this.right.update(1500 - this.speed);
+            this.right.setSpeed(1500 - this.speed);
         } else {
-            this.left.update(1450);
-            this.right.update(1450);
+            this.left.setSpeed(1450);
+            this.right.setSpeed(1450);
             BoeBot.wait(775);
-            this.left.update(1500);
-            this.right.update(1500);
+            this.left.setSpeed(1500);
+            this.right.setSpeed(1500);
         }
     }
 
     public void turnRight() {
 
         if (this.speed > 0) {
-            this.left.update(1550 + this.speed);
+            this.left.setSpeed(1550 + this.speed);
             BoeBot.wait(850);
-            this.left.update(1500 + this.speed);
+            this.left.setSpeed(1500 + this.speed);
         } else {
-            this.left.update(1550);
-            this.right.update(1550);
+            this.left.setSpeed(1550);
+            this.right.setSpeed(1550);
             BoeBot.wait(775);
-            this.left.update(1500);
-            this.right.update(1500);
+            this.left.setSpeed(1500);
+            this.right.setSpeed(1500);
         }
     }
 
     public void handBreak() {
-        this.right.update(1500);
-        this.left.update(1500);
+        this.right.setSpeed(1500);
+        this.left.setSpeed(1500);
     }
 
     public void increaseSpeed() {
@@ -186,29 +183,5 @@ public class Drive{
             this.speed -= 50;
             decelerate(oldSpeed);
         }
-    }
-
-    public boolean isForwards() {
-        return forwards;
-    }
-
-    public void setForwards(boolean forwards) {
-        this.forwards = forwards;
-    }
-
-    public boolean isBackwards() {
-        return backwards;
-    }
-
-    public void setBackwards(boolean backwards) {
-        this.backwards = backwards;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
     }
 }
