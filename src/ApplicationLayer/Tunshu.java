05eeba648979adfb9901075b4679f4faafbec5fd
rpvos <rik.vos01@gmail.com>
2019-11-25 13:01:03 +0100

    main netter gemaakt

--- src/ApplicationLayer/Tunshu.java
+++ src/ApplicationLayer/Tunshu.java
@@ -1,13 +1,17 @@
 package ApplicationLayer;
 
 import HardwareLayer.RemoteControl;
-import InterfaceLayer.Drive;
-import InterfaceLayer.NotificationSystem;
+import InterfaceLayer.*;
+import InterfaceLayer.Override;
 import TI.BoeBot;
 
 public class Tunshu {
     private Drive drive;
+    private HitDetection hitDetection;
     private NotificationSystem notificationSystem;
+    private InterfaceLayer.Override override;
+    private Route route;
+    private RouteFollower routeFollower;
 
     public void start() {
         //initialise every object
@@ -30,7 +34,18 @@ public class Tunshu {
      */
     public void init(){
         this.drive = new Drive(13, 12);
+        this.hitDetection = new HitDetection();
         this.notificationSystem = new NotificationSystem();
+        /**
+         * @todo
+         * hitDetection
+         * override
+         * route
+         * routeFollowe
+         */
+        this.override = new Override();
+        this.route = new Route();
+        this.routeFollower = new RouteFollower();
     }
 
 
