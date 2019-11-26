271fc9d6147376763177a54e1dcff2f39e9c94d6
rpvos <rik.vos01@gmail.com>
2019-11-26 12:24:28 +0100

    Ultrasone callback in hitdetection

--- src/InterfaceLayer/HitDetection.java
+++ src/InterfaceLayer/HitDetection.java
@@ -1,15 +1,21 @@
 package InterfaceLayer;
 
+import HardwareLayer.Sensor.Ultrasonic;
 import HardwareLayer.Sensor.UltrasonicCallBack;
 import HeadInterfaces.Updatable;
 
 public class HitDetection implements Updatable, UltrasonicCallBack {
 
     private double ultraSonicDistance;
+    private Ultrasonic ultrasone;
+
+    public HitDetection() {
+        this.ultrasone = new Ultrasonic(this);
+    }
 
     @java.lang.Override
     public void update() {
-
+        ultrasone.update();
     }
 
     public void ultrasonicSensorDistance(double distance){
