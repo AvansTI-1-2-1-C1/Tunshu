0649f63dbf47a7768f110b3a800df2aad06a571b
rpvos <rik.vos01@gmail.com>
2019-11-27 15:59:22 +0100

    comments

--- src/HardwareLayer/RemoteControlCallBack.java
+++ src/HardwareLayer/RemoteControlCallBack.java
@@ -1,5 +1,7 @@
 package HardwareLayer;
 
 public interface RemoteControlCallBack {
+    //creates a contract that states, if implemented the methods here WILL be part of the class
+    //we use this to create callbacks
     void onButtonPress(int buttonPress);
 }
