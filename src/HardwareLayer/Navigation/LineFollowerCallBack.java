ab4020d95db931ea1c47de4ea0a5a59ae042afc4
stijn <lemmstijn@hotmail.com>
2019-11-28 12:21:25 +0100

    Commends and stuff

--- src/HardwareLayer/Navigation/LineFollowerCallBack.java
+++ src/HardwareLayer/Navigation/LineFollowerCallBack.java
@@ -1,5 +1,7 @@
 package HardwareLayer.Navigation;
 
 public interface LineFollowerCallBack {
+
+    //This is the contract that all classes that implement this interface have the following method
     void onLineFollowerStatus(double leftLineFollowerCallBack, double middleLineFollowerCallBack, double rightLineFollowerCallBack);
 }
