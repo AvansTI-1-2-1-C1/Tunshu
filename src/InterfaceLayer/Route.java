c51eaef324d1a1df64fc4182123abad414010ac0
Rik <rik.vos01@gmail.com>
2019-12-11 12:42:26 +0100

    string conversion

--- src/InterfaceLayer/Route.java
+++ src/InterfaceLayer/Route.java
@@ -6,23 +6,23 @@ import java.util.ArrayList;
 
 public class Route implements Updatable {
 
-    private ArrayList<Character> directions;
+    private ArrayList<String> directions;
     private int instructionCounter;
 
     public Route() {
         this.instructionCounter = 0;
     }
 
-    public ArrayList<Character> getDirections() {
+    public ArrayList<String> getDirections() {
         return directions;
     }
 
-    public void setDirections(ArrayList<Character> directions) {
+    public void setDirections(ArrayList<String> directions) {
         this.directions = directions;
     }
 
-    public Character getDirection(){
-        char direction = directions.get(instructionCounter);
+    public String getDirection(){
+        String direction = directions.get(instructionCounter);
         this.instructionCounter = this.instructionCounter+1;
         return direction;
     }
