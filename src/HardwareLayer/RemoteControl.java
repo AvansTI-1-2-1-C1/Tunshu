0104219bfa2c29f575266848c33d7aabcd74b433
Thaomas <thaomas35@gmail.com>
2019-12-12 11:19:32 +0100

    2de ir reader

--- src/HardwareLayer/RemoteControl.java
+++ src/HardwareLayer/RemoteControl.java
@@ -14,18 +14,15 @@ public class RemoteControl implements Updatable, Switchable {
     }
 
     public void update() {
-
-        int pin = 0;
         int[] binaryInput = new int[12];
-        int pulseLen = BoeBot.pulseIn(pin, false, 6000);
 
         // if the puls length is longer then 2000 its the starting bit.
-        if (pulseLen > 2000) {
+        if (BoeBot.pulseIn(0, false, 6000) > 2000) {
             int[] lengths = new int[12];
 
             // fill 12 slots of the array in reversed order.
             for (int i = 11; i >= 0; i--) {
-                lengths[i] = BoeBot.pulseIn(pin, false, 20000);
+                lengths[i] = BoeBot.pulseIn(0, false, 20000);
             }
 
             // turns the digits into 1 and 0 according to the length.
@@ -36,11 +33,25 @@ public class RemoteControl implements Updatable, Switchable {
                     binaryInput[i] = 1;
                 }
             }
-            remoteControlCallBack.onButtonPress(convertBinary(binaryInput));
+        } else if (BoeBot.pulseIn(4, false, 6000) > 2000) {
+            int[] lengths = new int[12];
+
+            // fill 12 slots of the array in reversed order.
+            for (int i = 11; i >= 0; i--) {
+                lengths[i] = BoeBot.pulseIn(4, false, 20000);
+            }
+
+            // turns the digits into 1 and 0 according to the length.
+            for (int i = 0; i < 12; i++) {
+                if (lengths[i] < 900) {
+                    binaryInput[i] = 0;
+                } else {
+                    binaryInput[i] = 1;
+                }
+            }
         }
+        remoteControlCallBack.onButtonPress(convertBinary(binaryInput));
     }
-
-
 
 
     public static int convertBinary(int[] numbers) {
