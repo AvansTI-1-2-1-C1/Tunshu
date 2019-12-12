e0da72485c11f0de4ad7a3df967c3e7c8913cb6c
Thaomas <thaomas35@gmail.com>
2019-12-12 09:04:50 +0100

    remote update

--- src/InterfaceLayer/Override.java
+++ src/InterfaceLayer/Override.java
@@ -13,7 +13,7 @@ public class Override implements Updatable, RemoteControlCallBack, BluetoothCall
     private RemoteControl remoteControl;
     private Bluetooth bluetooth;
 
-    private String sellectedCommand;
+    private String selectedCommand;
     private String previousCommand;
     private MotorControl motorControl;
     private NotificationSystem notificationSystem;
@@ -22,30 +22,30 @@ public class Override implements Updatable, RemoteControlCallBack, BluetoothCall
     private RouteFollower routeFollower;
 
     public Override(MotorControl motorControl, NotificationSystem notificationSystem, HitDetection hitDetection, RouteFollower routeFollower) {
-//        this.remoteControl = new RemoteControl(this);
+        this.remoteControl = new RemoteControl(this);
         this.motorControl = motorControl;
         this.bluetooth = new Bluetooth(this);
         this.notificationSystem = notificationSystem;
         this.hitDetection = hitDetection;
         this.inputDelay = new Timer(250);
         this.routeFollower = routeFollower;
-        this.sellectedCommand = "";
+        this.selectedCommand = "";
         this.previousCommand = "";
     }
 
     public void useButton() {
         //check if the selected button was pressed right before by checking it against the previous button code and the timer
-        if (this.sellectedCommand.equals(this.previousCommand)) {
+        if (this.selectedCommand.equals(this.previousCommand)) {
             if (!this.inputDelay.timeout()) {
                 return;
             }
         } else {
-            this.previousCommand = this.sellectedCommand;
+            this.previousCommand = this.selectedCommand;
         }
 
 
         //switch statement that selects the corresponding method
-        switch (sellectedCommand) {
+        switch (selectedCommand) {
             case "forward":
                 this.motorControl.setMotorsTarget(0.2f, 0f);
                 break;
@@ -71,15 +71,15 @@ public class Override implements Updatable, RemoteControlCallBack, BluetoothCall
                 this.notificationSystem.mute();
                 break;
             case "LineFollower":
-                if (this.routeFollower.isOn()){
+                if (this.routeFollower.isOn()) {
                     this.routeFollower.off();
-                }else {
+                } else {
                     this.routeFollower.on();
                 }
                 break;
             case "handbreak":
                 //make sure there is nothing detected
-                if (!this.hitDetection.getState()){
+                if (!this.hitDetection.getState()) {
                     this.motorControl.setHandBreak(false);
                 }
                 break;
@@ -89,24 +89,90 @@ public class Override implements Updatable, RemoteControlCallBack, BluetoothCall
         }
 
         this.inputDelay.mark();
-        this.sellectedCommand = "";
+        this.selectedCommand = "";
     }
 
     @java.lang.Override
     public void update() {
-//        remoteControl.update();
+        this.remoteControl.update();
         this.bluetooth.update();
         useButton();
-
     }
 
     @java.lang.Override
     public void onButtonPress(int buttonPress) {
-        //TODO make something covert to sellectedCommand
+        //TODO make something covert to selectedCommand
+        switch (buttonPress) {
+            case 0:
+                break;
+
+            case 1:
+                System.out.println("Stop");
+                this.selectedCommand = "stop";
+                break;
+
+            case 144:
+                // Boebot gaat vooruit
+                System.out.println("Vooruit");
+                this.selectedCommand = "forward";
+                break;
+
+            case 2192:
+                //Boebot gaat achteruit
+                System.out.println("Reverse");
+                this.selectedCommand = "backward";
+                notificationSystem.setStatus("reverse", false);
+                break;
+
+            case 3216:
+                //Boebot turns left
+                System.out.println("Links");
+                this.selectedCommand = "left";
+                break;
+
+            case 1168:
+                //Boebot turns right
+                System.out.println("Rechts");
+                this.selectedCommand = "right";
+                break;
+
+            case 2704:
+                //Stop
+                System.out.println("Fullstop");
+                this.selectedCommand = "stop";
+                break;
+
+            case 1936:
+                //Faster
+                System.out.println("Sneller");
+                this.selectedCommand = "faster";
+                break;
+
+            case 3984:
+                //Slower
+                System.out.println("Langzamer");
+                this.selectedCommand = "slower";
+                break;
+
+            case 656:
+                //Mute speaker
+                System.out.println("mute");
+                this.notificationSystem.mute();
+                break;
+
+            case 16:
+                //Enable/Disable lineFollower
+                System.out.println("LineFollower");
+                this.selectedCommand = "LineFollower";
+                break;
+
+            default:
+                break;
+        }
     }
 
     @java.lang.Override
     public void onInput(String command) {
-        this.sellectedCommand = command;
+        this.selectedCommand = command;
     }
 }
